7fe5e4ea7fb446c25de343d53e29f616a1cc7587
Jason Chang <jasonchang1712@gmail.com>
2026-04-06 20:34:59 -0500

    Add stage and file format to DDL.

--- src/ddl.sql
+++ src/ddl.sql
@@ -1,1 +1,17 @@
 create schema edwprodhh.iso;
+
+create stage edwprodhh.iso.stg_response;
+
+
+-- snowsql -q "PUT file://\\\\hh-fileserver01\\TempUL2\\IU_Health_Complex\\837_FILES_IN\\2026\\*_i*.837 @edwprodhh.iso.stg_response auto_compress=false;"
+list @edwprodhh.iso.stg_response;
+
+create or replace file format
+    edwprodhh.iso.format_txt
+type                            = 'CSV'
+field_delimiter                 = '\u0000'  -- an impossible delimiter; treats entire row as one value.
+record_delimiter                = '\n'
+skip_header                     = 0
+field_optionally_enclosed_by    = NONE
+escape_unenclosed_field         = NONE
+;
