3dc78bdc513daf76ba228a61f5595e9156c5c60b
Jason Chang <jasonchang1712@gmail.com>
2026-09-25 09:09:09 -0500

    Add batch and balance filters for ISO requests.

--- src/request.sql
+++ src/request.sql
@@ -64,7 +64,9 @@ begin
                             end
                     and debtor.packet_idx   not in (select packet_idx from previous_packets)
                     and dimdebtor.drl       not in (select drl from previous_packets)
-                    and debtor.batch_date >= '2025-01-01'
+                    and debtor.batch_date >= '2026-07-01'
+                    and debtor.balance_dimdebtor >= 25
+                    and debtor.assigned >= 25
                     --** Need to exclude based on values in F303-F310 and F323-330. Waiting on Dan and Heather
         qualify     row_number() over (partition by debtor.packet_idx order by debtor.debtor_idx desc) = 1
         order by    debtor.batch_date desc, debtor.balance_dimdebtor desc
